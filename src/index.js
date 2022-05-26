91dd8184abe60e991df4f526df0af891b34687e9
Juan <jmespana01@gmail.com>
2022-05-25 19:45:35 -0600

    Search and filter products, added IndexRouter

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import IndexRouter from './routes/IndexRouters';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+      <IndexRouter/>
+);
+
---
+++ src/routes/IndexRouters.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+import App from '../App';
+import NavBar from '../Components/NavBar';
+import NotFound from '../Components/NotFound';
+import ProductList from '../Components/ProductList';
+import CategoryList from '../Components/CategoryList';
+
+import ProductById from './ProductById';
+import ProductsByCategory from './ProductsByCategory';
+
+import {
+    Route,
+    Routes,  
+    useParams,
+    BrowserRouter,
+  } from "react-router-dom";
+
+class IndexRouter extends Component {
+    constructor() {
+      super();
+    }
+
+    render(){
+        return (
+            <BrowserRouter>
+            <NavBar/>
+            <Routes>
+                <Route path="/" element={<App/>} />          
+                <Route path="products" element={<ProductList random={true} cols="3" limit="6"/>} />
+                <Route path="products/:productId" element={<ProductById />} />
+                <Route path="categories" element={<CategoryList />} />
+                <Route path="categories/:categoryName" element={<ProductsByCategory />} />
+                <Route path="*" element={<NotFound/>} />
+            </Routes>
+            </BrowserRouter>
+        )
+    }
+
+}
+export default IndexRouter;
